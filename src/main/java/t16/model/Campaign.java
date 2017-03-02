3abb5ad77e2737e0c036762309c64f64823ba9d7
Huw Jones <huwcbjones@gmail.com>
2017-03-02 01:04:24 +0000

    Fixed campaign data type

--- src/main/java/t16/model/Campaign.java
+++ src/main/java/t16/model/Campaign.java
@@ -15,10 +15,35 @@ import java.util.HashMap;
  * TODO Here is a possible format for the Campaign. What do you think? After calculating the axes for seconds, there could be some additional calculation to get the average y values for minutes, hours etc.
  */
 public class Campaign {
+    public HashMap<Interval, AxisPair> data;
     private String name;
 
-    public enum Interval
-    {
+    public Campaign(File dbFile) {
+        this.name = dbFile.getName();
+    }
+
+    public void setData(String name, ResultSet results, boolean isFloat) throws SQLException {
+        this.data = new HashMap<>();
+        ArrayList<String> x = new ArrayList<>();
+        ArrayList<Number> y = new ArrayList<>();
+        results.next();
+        while (!results.isLast()) {
+            x.add(results.getString(1));
+            if (isFloat) {
+                y.add(results.getFloat(2));
+            } else {
+                y.add(results.getInt(2));
+            }
+            results.next();
+        }
+        this.data.put(Interval.SECONDS, new AxisPair(x, y));
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public enum Interval {
         SECONDS,
         MINUTES,
         HOURS,
@@ -27,51 +52,21 @@ public class Campaign {
         MONTHS,
     }
 
-    public Campaign(File dbFile)
-    {
-        this.name = dbFile.getName();
-    }
-
-    public HashMap<Interval, AxisPair> data;
-
-    public void setData(String name, ResultSet results) throws SQLException
-    {
-		this.data = new HashMap<>();
-		ArrayList<String> x = new ArrayList<>();
-		ArrayList<Integer> y = new ArrayList<>();
-        results.next();
-		while(!results.isLast())
-		{
-			x.add(results.getString(1));
-			y.add(results.getInt(2));
-			results.next();
-		}
-		this.data.put(Interval.SECONDS, new AxisPair(x, y));
-    }
-
-    public String getName()
-    {
-        return this.name;
-    }
-
-    public class AxisPair
-    {
+    public class AxisPair {
         private ArrayList<String> xAxis;
-		private ArrayList<Integer> yAxis;
-		private AxisPair(ArrayList<String> xAxis, ArrayList<Integer> yAxis)
-		{
-			this.xAxis = xAxis;
-			this.yAxis = yAxis;
-		}
-		
-		public ArrayList<String> getXAxis()
-		{
-			return this.xAxis;
-		}
-			
-		public ArrayList<Integer> getYAxis()
-		{
-			return this.yAxis;
-		}
+        private ArrayList<Number> yAxis;
+
+        private AxisPair(ArrayList<String> xAxis, ArrayList<Number> yAxis) {
+            this.xAxis = xAxis;
+            this.yAxis = yAxis;
+        }
+
+        public ArrayList<String> getXAxis() {
+            return this.xAxis;
+        }
+
+        public ArrayList<Number> getYAxis() {
+            return this.yAxis;
+        }
     }
 }
