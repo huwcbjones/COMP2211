9b3ae402d5bb4ec849496c0b73d41bc967e884f8
Huw Jones <huwcbjones@gmail.com>
2017-03-25 11:24:40 +0000

    Added methods to set each individual statistic. Converted to VBox

--- src/main/java/t16/controller/StatsControl.java
+++ src/main/java/t16/controller/StatsControl.java
@@ -3,21 +3,22 @@ package t16.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import t16.model.Campaign;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.util.Locale;
 
 /**
- * {DESCRIPTION}
+ * Controller for Statistics View
  *
  * @author Huw Jones
  * @since 14/03/2017
  */
-public class StatsControl extends GridPane {
+public class StatsControl extends VBox {
 
+    //<editor-fold desc="View Controls">
     @FXML
     private Label numberImpressions;
 
@@ -50,9 +51,11 @@ public class StatsControl extends GridPane {
 
     @FXML
     private Label bounceRate;
+    //</editor-fold>
 
     private NumberFormat nf = NumberFormat.getNumberInstance(Locale.getDefault());
     private NumberFormat cf = NumberFormat.getCurrencyInstance(Locale.UK);
+    private NumberFormat pf = NumberFormat.getPercentInstance(Locale.getDefault());
 
     public StatsControl() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/StatsControl.fxml"));
@@ -66,23 +69,51 @@ public class StatsControl extends GridPane {
         }
     }
 
-    public void setCampaign(Campaign c) {
-        if (c == null) return;
-        if (numberImpressions != null) numberImpressions.setText(nf.format(c.getNumberImpressions()));
-        if (numberClicks != null) numberClicks.setText(nf.format(c.getNumberClicks()));
-        if (numberUniques != null) numberUniques.setText(nf.format(c.getNumberUniques()));
-        if (numberConversions != null) numberConversions.setText(nf.format(c.getNumberConversions()));
-        if (numberBounces != null) numberBounces.setText(nf.format(Dashboard.BOUNCE_DEFINITION ? c.getNumberBouncesPages() : c.getNumberBouncesTime()));
+    public void setNumberImpressions(long numberImpressions) {
+        this.numberImpressions.setText(nf.format(numberImpressions));
+    }
 
-        if (totalCost != null) totalCost.setText(cf.format(c.getTotalCost()));
-        if (costPerClick != null) costPerClick.setText(cf.format(c.getCostPerClick()));
-        if (costPerAcquisition != null) {
-            if(c.getCostPerAcquisition() != null) costPerAcquisition.setText(cf.format(c.getCostPerAcquisition()));
-        }
-        if (costPer1kImpressions != null) costPer1kImpressions.setText(cf.format(c.getCostPer1kImpressions()));
+    public void setNumberClicks(long numberClicks) {
+        this.numberClicks.setText(nf.format(numberClicks));
+    }
 
+    public void setNumberUniques(long numberUniques) {
+        this.numberUniques.setText(nf.format(numberUniques));
+    }
 
-        if (clickThroughRate != null) clickThroughRate.setText(nf.format(c.getClickThroughRate()) + "%");
-        if (bounceRate != null) bounceRate.setText(nf.format(Dashboard.BOUNCE_DEFINITION ? c.getBounceRatePages() : c.getBounceRateTime()) + "%");
+    public void setNumberConversions(long numberConversions) {
+        this.numberConversions.setText(nf.format(numberConversions));
+    }
+
+    public void setNumberBounces(Label numberBounces) {
+        this.numberBounces = numberBounces;
+    }
+
+    public void setTotalCost(BigDecimal totalCost) {
+        if(totalCost == null) totalCost = BigDecimal.ZERO;
+        this.totalCost.setText(cf.format(totalCost));
+    }
+
+    public void setCostPerClick(BigDecimal costPerClick) {
+        if(costPerClick == null) costPerClick = BigDecimal.ZERO;
+        this.costPerClick.setText(cf.format(costPerClick));
+    }
+
+    public void setCostPerAcquisition(BigDecimal costPerAcquisition) {
+        if(costPerAcquisition == null) costPerAcquisition = BigDecimal.ZERO;
+        this.costPerAcquisition.setText(cf.format(costPerAcquisition));
+    }
+
+    public void setCostPer1kImpressions(BigDecimal costPer1kImpressions) {
+        if(costPer1kImpressions == null) costPer1kImpressions = BigDecimal.ZERO;
+        this.costPer1kImpressions.setText(cf.format(costPer1kImpressions));
+    }
+
+    public void setClickThroughRate(double clickThroughRate) {
+        this.clickThroughRate.setText(pf.format(clickThroughRate));
+    }
+
+    public void setBounceRate(double bounceRate) {
+        this.bounceRate.setText(pf.format(bounceRate));
     }
 }
