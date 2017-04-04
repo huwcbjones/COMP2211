a2684150585182c94fca7b0c72ad675a45fb405e
Huw Jones <huwcbjones@gmail.com>
2017-04-04 23:05:26 +0100

    Logging when stats tasks complete

--- src/main/java/t16/controller/Dashboard.java
+++ src/main/java/t16/controller/Dashboard.java
@@ -284,6 +284,7 @@ public class Dashboard {
                 impressionsTask.setOnSucceeded(event -> {
                     statsPanel.setNumberImpressions((long) event.getSource().getValue());
                     latch.countDown();
+                    log.info("Impressions loaded");
                 });
                 taskList.add(impressionsTask);
 
@@ -296,6 +297,7 @@ public class Dashboard {
                 clicksTask.setOnSucceeded(event -> {
                     statsPanel.setNumberClicks((long) event.getSource().getValue());
                     latch.countDown();
+                    log.info("Clicks loaded");
                 });
                 taskList.add(clicksTask);
 
@@ -308,6 +310,7 @@ public class Dashboard {
                 uniquesTask.setOnSucceeded(event -> {
                     statsPanel.setNumberUniques((long) event.getSource().getValue());
                     latch.countDown();
+                    log.info("Uniques loaded");
                 });
                 taskList.add(uniquesTask);
 
@@ -320,6 +323,7 @@ public class Dashboard {
                 conversionsTask.setOnSucceeded(event -> {
                     statsPanel.setNumberConversions((long) event.getSource().getValue());
                     latch.countDown();
+                    log.info("Conversions loaded");
                 });
                 taskList.add(conversionsTask);
 
@@ -332,6 +336,7 @@ public class Dashboard {
                 bouncesTask.setOnSucceeded(event -> {
                     statsPanel.setNumberBounces((long) event.getSource().getValue());
                     latch.countDown();
+                    log.info("Bounces loaded");
                 });
                 taskList.add(bouncesTask);
 
@@ -344,6 +349,7 @@ public class Dashboard {
                 totalCostTask.setOnSucceeded(event -> {
                     statsPanel.setTotalCost((BigDecimal) event.getSource().getValue());
                     latch.countDown();
+                    log.info("Total Cost loaded");
                 });
                 taskList.add(totalCostTask);
 
@@ -356,6 +362,7 @@ public class Dashboard {
                 costPerClickTask.setOnSucceeded(event -> {
                     statsPanel.setCostPerClick((BigDecimal) event.getSource().getValue());
                     latch.countDown();
+                    log.info("Cost per Click loaded");
                 });
                 taskList.add(costPerClickTask);
 
@@ -368,6 +375,7 @@ public class Dashboard {
                 costPerAcquisitionTask.setOnSucceeded(event -> {
                     statsPanel.setCostPerAcquisition((BigDecimal) event.getSource().getValue());
                     latch.countDown();
+                    log.info("Cost per Acquisition loaded");
                 });
                 taskList.add(costPerAcquisitionTask);
 
@@ -380,6 +388,7 @@ public class Dashboard {
                 costPer1kTask.setOnSucceeded(event -> {
                     statsPanel.setCostPer1kImpressions((BigDecimal) event.getSource().getValue());
                     latch.countDown();
+                    log.info("Cost per 1k Impressions loaded");
                 });
                 taskList.add(costPer1kTask);
 
@@ -392,6 +401,7 @@ public class Dashboard {
                 clickThruTask.setOnSucceeded(event -> {
                     statsPanel.setClickThroughRate((double) event.getSource().getValue());
                     latch.countDown();
+                    log.info("Click Through Rate loaded");
                 });
                 taskList.add(clickThruTask);
 
@@ -405,6 +415,7 @@ public class Dashboard {
                 bounceRateTask.setOnSucceeded(event -> {
                     statsPanel.setBounceRate((double) event.getSource().getValue());
                     latch.countDown();
+                    log.info("Bounce Rate loaded");
                 });
                 taskList.add(bounceRateTask);
 
