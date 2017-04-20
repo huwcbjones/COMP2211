7daaec5d9ce64466c1a4036e1937e9dbc762b45b
James Curran <jc27g15@soton.ac.uk>
2017-04-20 22:30:15 +0100

    Rolling over a node now displays its y value
    Clicking the node "locks" the value on screen
    Clicking again "unlocks" it
    Using these special nodes by default, but should be a customisation
    option
    Graphs are now filtered by day, on Abdullah's request

--- src/main/java/t16/model/Chart.java
+++ src/main/java/t16/model/Chart.java
@@ -2,10 +2,16 @@ package t16.model;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.util.Pair;
 
 import java.util.HashMap;
@@ -34,7 +40,8 @@ public class Chart {
     }
 
     public void addSeries(String seriesName, List<Pair<String, Number>> data) {
-        Series s= new Series(seriesName, data);
+        //It is recommended that the true is replaced with some customisation variable
+        Series s= new Series(seriesName, data, true);
         series.put(seriesName, s.getSeries());
         seriesData.add(s.getSeries());
     }
@@ -67,11 +74,32 @@ public class Chart {
 
         private XYChart.Series<String, Number> series;
 
-        public Series(String seriesName, List<Pair<String, Number>> data) {
+        /**
+         * If useExactNodes is false, the graph produced will be the same as before.
+         * If true, each node in the graph will be an ExactDataNode (see below).
+         * This does instantiate an ExactDataNode for every data point in the chart so performance might take a hit.
+         * However, supervisor has mentioned before that they want something along the lines of this.
+         * I recommend that there be an option somewhere in the application to toggle useExactNodes.
+         */
+        public Series(String seriesName, List<Pair<String, Number>> data, boolean useExactNodes) {
             series = new XYChart.Series<>();
             series.setName(seriesName);
-            for (Pair<String, Number> p : data) {
-                series.getData().add(new XYChart.Data<>(p.getKey(), p.getValue()));
+            if(useExactNodes)
+            {
+                for (Pair<String, Number> p : data)
+                {
+                    Number n = p.getValue();
+                    XYChart.Data element = new XYChart.Data<>(p.getKey(), n);
+                    element.setNode(new Chart.ExactDataNode(n));
+                    series.getData().add(element);
+                }
+            }
+            else
+            {
+                for (Pair<String, Number> p : data)
+                {
+                    series.getData().add(new XYChart.Data<>(p.getKey(), p.getValue()));
+                }
             }
         }
 
@@ -80,5 +108,62 @@ public class Chart {
         }
     }
 
+    /**
+     * Rolling over an ExactDataNode displays its y value. Rolling off makes the value disappear.
+     * Clicking the node "locks" the value onto the screen.
+     * Clicking it again will "unlock" it.
+     */
+    public class ExactDataNode extends StackPane
+    {
+        private boolean selected;
 
+        public ExactDataNode(Number value)
+        {
+            super();
+            this.setPrefSize(10, 10);
+            final Label label = this.createLabel(value);
+            this.selected = false;
+            this.setOnMouseEntered(new EventHandler<MouseEvent>()
+            {
+                @Override
+                public void handle(MouseEvent mouseEvent)
+                {
+                    if(!selected)
+                    {
+                        getChildren().setAll(label);
+                        toFront();
+                        //ExactDataNode.this.setStyle("");
+                    }
+                }
+            });
+
+            this.setOnMouseExited(new EventHandler<MouseEvent>()
+            {
+                @Override
+                public void handle(MouseEvent mouseEvent)
+                {
+                    if(!selected)
+                    {
+                        getChildren().clear();
+                    }
+                }
+            });
+
+            this.setOnMouseClicked(new EventHandler<MouseEvent>()
+            {
+                @Override
+                public void handle(MouseEvent event) {
+                    selected = !selected;
+                }
+            });
+        }
+
+        private Label createLabel(Number value) {
+            final Label label = new Label(value.toString());
+            label.setStyle("-fx-font-size: 10; -fx-font-weight: bold;");
+            label.setTextFill(Color.DARKBLUE);
+            label.setMinSize(Label.USE_PREF_SIZE, Label.USE_PREF_SIZE);
+            return label;
+        }
+    }
 }
